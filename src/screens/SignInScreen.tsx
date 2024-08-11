1b23b963584e84ac564cce1c8bf314c378cfc98f
Nova136 <chiayihang@gmail.com>
2024-08-11 18:02:43 +0800

    shared styling for all 3 pages

--- src/screens/SignInScreen.tsx
+++ src/screens/SignInScreen.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, StyleSheet, Pressable } from "react-native";
+import { View, Image, Text, TextInput, Pressable } from "react-native";
 import { Auth } from "aws-amplify";
+import { useSharedStyles } from "../style/sharedStyles";
 
 export default function SignInScreen({ navigation }) {
+  const styles = useSharedStyles();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
@@ -18,63 +20,31 @@ export default function SignInScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
+      <Image source={require("../assets/unirent-logo.png")} style={styles.logo} />
       <Text style={styles.title}>Sign In</Text>
-      {error ? <Text style={styles.error}>{error}</Text> : null}
+      {error && <Text style={styles.error}>{error}</Text>}
       <TextInput
-        style={styles.input}
         placeholder="Username"
         value={username}
         onChangeText={setUsername}
+        style={styles.input}
       />
       <TextInput
-        style={styles.input}
         placeholder="Password"
         value={password}
         onChangeText={setPassword}
         secureTextEntry
+        style={styles.input}
       />
-
-      <Pressable onPress={signIn} style={styles.pressable}>
+      <Pressable onPress={signIn} style={styles.button}>
         <Text style={styles.buttonText}>Sign In</Text>
       </Pressable>
-      <Pressable onPress={() => navigation.navigate("RegisterScreen")} style={styles.pressable}>
-        <Text style={styles.buttonText}>Register</Text>
+      <Pressable onPress={() => navigation.navigate("RegisterScreen")} style={styles.link}>
+        <Text style={styles.linkText}>Register</Text>
       </Pressable>
-      <Pressable onPress={() => navigation.navigate("ForgotPasswordScreen")} style={styles.pressable}>
-        <Text style={styles.buttonText}>Forgot Password</Text>
+      <Pressable onPress={() => navigation.navigate("ForgotPasswordScreen")} style={styles.link}>
+        <Text style={styles.linkText}>Forgot Password?</Text>
       </Pressable>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 20,
-  },
-  title: {
-    fontSize: 24,
-    marginBottom: 20,
-  },
-  input: {
-    borderWidth: 1,
-    padding: 10,
-    marginBottom: 10,
-    borderRadius: 5,
-  },
-  pressable: {
-    backgroundColor: "#2196F3",
-    padding: 10,
-    marginVertical: 10,
-    borderRadius: 5,
-    boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
-  },
-  buttonText: {
-    color: "white",
-    textAlign: "center",
-    verticalAlign: "middle",
-  },
-  error: {
-    color: "red",
-    marginBottom: 10,
-  },
-});
