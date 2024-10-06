8b18f82b148e358cb0e8e8176c97db4d1911791f
jayednchee97 <jaydenchee97@gmail.com>
2024-10-06 13:59:56 +0800

    add accommodationAPI setup

--- src/api/AccommodationAPI.tsx
+++ src/api/AccommodationAPI.tsx
@@ -50,3 +50,70 @@ export async function postUniAccommodation(request) {
     console.error("Error in post uni accommodation API: " + error);
   }
 }
+
+export async function updateAccommodation(request) {
+  console.log("Trigger post accommodation...");
+  const jwtToken = (await Auth.currentSession()).getAccessToken().getJwtToken();
+  const headers = {
+    Authorization: "Bearer " + jwtToken
+  };
+  const myInit = { headers: headers, body: request };
+  try {
+    const response = await API.put(apiName, "/accommodations", myInit);
+    return response;
+  } catch (error) {
+    console.error("Error in post accommodation API: " + error);
+  }
+}
+
+export async function updateUniAccommodation(request) {
+  console.log("Trigger post university accommodation...");
+  const jwtToken = (await Auth.currentSession()).getAccessToken().getJwtToken();
+  const headers = {
+    Authorization: "Bearer " + jwtToken
+  };
+
+  const myInit = { headers: headers, body: request };
+
+  try {
+    const response = await API.put(apiName, "/accommodations/university", myInit);
+    return response;
+  } catch (error) {
+    console.error("Error in post uni accommodation API: " + error);
+  }
+}
+
+export async function deleteAccommodation(accommodationId) {
+  console.log("Trigger post university accommodation...");
+  const jwtToken = (await Auth.currentSession()).getAccessToken().getJwtToken();
+  const headers = {
+    Authorization: "Bearer " + jwtToken
+  };
+
+  const myInit = { headers: headers };
+
+  try {
+    const response = await API.del(apiName, "/accommodations/university/" + accommodationId , myInit);
+    return response;
+  } catch (error) {
+    console.error("Error in post uni accommodation API: " + error);
+  }
+}
+
+export async function deleteUniAccommodation(accommodationId) {
+  console.log("Trigger post university accommodation...");
+  const jwtToken = (await Auth.currentSession()).getAccessToken().getJwtToken();
+  const headers = {
+    Authorization: "Bearer " + jwtToken
+  };
+
+  const myInit = { headers: headers };
+
+  try {
+    const response = await API.del(apiName, "/accommodations/university/" + accommodationId , myInit);
+    return response;
+  } catch (error) {
+    console.error("Error in post uni accommodation API: " + error);
+  }
+}
+
